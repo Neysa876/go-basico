128073eceaf4980a8e06fa84ce8c4e5bf0655387
Neysa Alarcon <neysa.alarcon@gmail.com>
2022-02-20 10:42:18 -0300

    feat: Operadores matematicos y logicos

--- main/main.go
+++ main/main.go
@@ -4,19 +4,31 @@ import "fmt"
 
 func main() {
 
-	var entero int
-	var decimal float64
-	var cadena string
-	var esReal bool
+	suma := 10 + 5
+	resta := 10 - 5
+	multiplicacion := 10 * 5
+	division := 10 / 5
+	resto := 10 % 5
 
-	entero = 8
-	decimal = 4.4
-	cadena = "Hola! que tal?"
-	esReal = true
+	fmt.Println("Operadores matematicos", suma, resta, multiplicacion, division, resto)
 
-	fmt.Println("Primer intento", entero, decimal, cadena, esReal)
+	suma++
+	fmt.Println("Aumento de var suma", suma)
 
-	numerito := 80
+	suma--
+	fmt.Println("Decremento de var suma", suma)
 
-	fmt.Println("numerito", numerito)
+	suma += 10
+	fmt.Println("Aumento con parametro de var suma", suma)
+
+	oper1 := 10 < 5
+	oper2 := 10 > 5
+	oper3 := 10 <= 5
+	oper4 := 10 >= 5
+	oper5 := 10 == 5
+	oper6 := 10 != 5
+	oper7 := 10 > 5 && 10 < 30
+	oper8 := 10 < 7 || 10 > 2
+
+	fmt.Println("Operadores lógicos", oper1, oper2, oper3, oper4, oper5, oper6, oper7, oper8)
 }
