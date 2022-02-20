19d6efd725c6658902f2b5724822a121cb4fa9b6
Neysa Alarcon <neysa.alarcon@gmail.com>
2022-02-20 10:26:00 -0300

    feat: added file main and how to use variables in golang

---
+++ main/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var entero int
+	var decimal float64
+	var cadena string
+	var esReal bool
+
+	entero = 8
+	decimal = 4.4
+	cadena = "Hola! que tal?"
+	esReal = true
+
+	fmt.Println("Primer intento", entero, decimal, cadena, esReal)
+
+	numerito := 80
+
+	fmt.Println("numerito", numerito)
+}
