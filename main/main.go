ea1449c6e55a306f5d4f2d822fb479ebf2f60bdb
Neysa Alarcon <neysa.alarcon@gmail.com>
2022-02-20 16:27:03 -0300

    feat: Ejercicio numeros pares

--- main/main.go
+++ main/main.go
@@ -3,14 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i <= 10; i++ {
-		fmt.Println(i)
-	}
-
-	nombre := "Betito"
-	fmt.Println("Obten una letra de Betito", string(nombre[2]))
-
-	for i := 0; i < 7; i++ {
-		fmt.Println("Valor indice", string(nombre[i]))
+	for i := 0; i <= 20; i++ {
+		par := i % 2
+		if par == 0 {
+			fmt.Println("Numero par: ", i)
+		}
 	}
 }
